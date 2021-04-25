981b66f4004395eadc0f0cef4c5a2854b8b823b1
TuyenNguyenDinh <tuyennguyendinh1608@gmail.com>
2021-04-25 09:53:37 +0700

    add language Vietnam and English

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       
+        $data['categories'] = Categories::all();
+        view()->share($data);
     }
 }
